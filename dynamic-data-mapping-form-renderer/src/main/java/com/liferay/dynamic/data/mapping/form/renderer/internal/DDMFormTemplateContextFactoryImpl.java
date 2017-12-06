e238670bf1add7b7f73eb07e73997d5e835359d4
gregory-bretall <gregory.bretall@liferay.com>
2017-12-06 10:00:35 -0800

    LPS-76247 Add the ProxyPath to the beginning of references to ServletContextPath to properly account for when proxies are used.

--- dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormTemplateContextFactoryImpl.java
+++ dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormTemplateContextFactoryImpl.java
@@ -34,6 +34,7 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.AggregateResourceBundle;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -192,9 +193,11 @@ public class DDMFormTemplateContextFactoryImpl
 	}
 
 	protected String getDDMFormContextProviderServletURL() {
+		String proxyPath = _portal.getPathProxy();
+
 		String servletContextPath = getServletContextPath();
 
-		return servletContextPath.concat(
+		return proxyPath.concat(servletContextPath).concat(
 			"/dynamic-data-mapping-form-context-provider/");
 	}
 
@@ -357,5 +360,8 @@ public class DDMFormTemplateContextFactoryImpl
 
 	@Reference
 	private JSONFactory _jsonFactory;
+
+	@Reference
+	private Portal _portal;
 
 }
